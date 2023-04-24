985aca5dc383d509b64d0234e119493e94090998
Kevin-10046 <lokevin@hotmail.com.tw>
2023-04-25 02:02:56 +0800

    add word

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,3 +1,8 @@
 export default function About() {
-  return <h1>About</h1>;
+  return <main>
+  <p>In this infinite cosmos, my name is just a tiny spark</p>
+  <p>But I am here to tell you my story, to share my heart</p>
+  <p>From the depths of my soul, I create and dream</p>
+  <p>And I hope my journey will inspire and gleam</p>
+</main>;
 }
