cb24828b75f9f18389bd9311424f8a54b85b7a57
Kevin-10046 <lokevin@hotmail.com.tw>
2023-04-25 01:47:58 +0800

    add 家園

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -4,7 +4,7 @@ import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: "Home", href: "/" },
+  { name: "家園", href: "/" },
   { name: "About", href: "/about" },
   { name: "Users", href: "/users" },
   { name: "Create User", href: "/create-user" },
